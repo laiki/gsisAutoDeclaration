cac4a2dc314c0af55ca5278baa51b08851a6dc6f
Wasilios Goutas <wasili@goutas.de>
2025-10-01 20:24:36 +0200

    class gsisDeclaration now is working fine.

--- gsisDeclaration.py
+++ gsisDeclaration.py
@@ -16,11 +16,12 @@ import pathlib
 import argparse
 import tempfile
 import shutil
+import requests
 
 #%% defaults
 
 GSIS_DEFAULTS = {
-      'download_dir' : pathlib.Path('./downloads').absolute()
+        'download_dir' : pathlib.Path('./downloads').absolute()
       , 'url'          : "https://dilosi.services.gov.gr/templates/YPDIL/create"
       , 'timeout'      : 15
       , 'retries'      : 3
@@ -42,11 +43,15 @@ class gsisGrabber:
         self.url      = url
         self.filename = filename
         self.timeout  = timeout
-        self.retries  = retries
+        self.retries  = int(retries)
         
         self.chrome_options = Options()
         self.chrome_options.add_argument("--incognito") # private mode
+        self.chrome_options.add_argument("--disable-popup-blocking")
+        self.chrome_options.add_argument("--no-sandbox")
+        self.chrome_options.add_argument("--disable-dev-shm-usage")
         self.tmpdir = tempfile.TemporaryDirectory()
+        pathlib.Path(self.tmpdir.name).mkdir(parents=True, exist_ok=True)
         self.chrome_options.add_experimental_option("prefs", {
             "download.default_directory": self.tmpdir.name,  # Zielordner
             "download.prompt_for_download": False,       # Kein Dialog
@@ -65,10 +70,21 @@ class gsisGrabber:
         
         self.getCode = getCode
         self.filepath = None
-        
+        self.fileurl  = None
         return
     
     def __del__(self):
+        self.cleanup()
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cleanup()
+        
+        
+    def cleanup(self):
+        self.driver.quit()
         self.tmpdir.cleanup()
         return
         
@@ -114,7 +130,6 @@ class gsisGrabber:
             
     def _authentificate(self) :
         try:    
-            #begin_label = self.driver.find_element(By.XPATH, "//span[contains(text(), 'Συνέχεια')]")
             begin_label = self.wait.until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Συνέχεια')]")))
             self._scroll_and_click(begin_label)
 
@@ -127,7 +142,7 @@ class gsisGrabber:
             ))
             afm_value   = afm_element.text.strip()
     
-            if afm_value != self.taxID:
+            if afm_value != str(self.taxID):
                 raise Exception(f"TaxID received {afm_value} differs from {self.taxID}")
                 
             submit_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Συνέχεια']")))
@@ -136,7 +151,8 @@ class gsisGrabber:
             
 
         except Exception as e:
-            raise Exception("error in authentificationAFM") from e
+            self.driver.save_screenshot("debug/debug_screenshot_authentification.png")
+            raise Exception("error in authentification. TaxIDs differ") from e
         return
     
     def _initForm(self):
@@ -265,29 +281,37 @@ class gsisGrabber:
     def _getSMSCode(self):
         code = None
         if self.getCode is None:
-            code = input("enter code:")
+            code = input("enter code: ")
         else:
             code = self.getCode()
         return code
     
     def _saveDocument(self):
-        download_button = self.driver.find_element(By.XPATH, "//a[contains(text(), 'Αποθήκευση')]")
-        self._scroll_and_click(download_button)
-
+        #download_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Αποθήκευση')]")))
+        #self._scroll_and_click(download_button)
+        link_element  = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//a[contains(@href, "pdf-download")]')))
+        self.fileurl = link_element.get_attribute("href")
         
-        downloaded = list( pathlib.Path(self.tempdir.name).glob('*') )
+        response = requests.get(self.fileurl, headers={"User-Agent": "Mozilla/5.0"})
+        if response.status_code == 200:
+            (pathlib.Path(self.tmpdir.name)/"declaration.pdf").write_bytes(response.content)
+        else:
+            raise Exception(f"failed downloading file from {self.fileurl}")
+
+        downloaded = list( pathlib.Path(self.tmpdir.name).glob('*') )
         if len(downloaded) != 1:
             print('failed downloading file', downloaded)
-            raise Exception('downnload failed')
+            raise Exception(f"error in processing file download. check download folder {self.tmpdir.name}")
             
-        dest = self.download_dir/self.filename if not self.filename is None else self.download_dir/downloaded[0]
-        
+        base_dest = self.download_dir/self.filename if not self.filename is None else self.download_dir/downloaded[0]
+        dest = base_dest
         idx = 1
         while dest.exists():
-            dest = dest.with_stem(dest.stem + f' ({idx})')
-            
-        downloaded[0].replace(dest)  # move fiel to destination location
-        self.filepath = dest
+            dest = dest.with_stem(base_dest.stem + f' ({idx})')
+            idx += 1
+        shutil.move(downloaded[0], dest)   
+        #downloaded[0].replace(dest)  # cannot  move accross drives :(
+        self.filepath = dest         
         return
     
     
@@ -311,7 +335,7 @@ class gsisGrabber:
             print('declaration failed')
             raise e
             
-        return self.filepath                  
+        return self.fileurl, self.filepath                  
 
     def _scroll_to(self, element, timeout=5):
         # Scrollen
@@ -342,6 +366,7 @@ class gsisGrabber:
 if __name__ == '__main__':
     debug_dir = pathlib.Path('./debug')
     shutil.rmtree(debug_dir, ignore_errors=True)
+    debug_dir.mkdir(parents=True, exist_ok=False)
     
     parser = argparse.ArgumentParser(
           prog='gsisDeclaration'
@@ -349,33 +374,41 @@ if __name__ == '__main__':
         )
     parser.add_argument('-u', '--user', dest='user',  default=None, required=True)
     parser.add_argument('-p', '--password', dest='password', default=None, required=True)
-    parser.add_argument('--taxid', dest='taxid',   default=None, required=True)
+    parser.add_argument('--taxid', dest='taxid',   default=None, type=int, required=True)
     parser.add_argument('--email', dest='email', default=None, required=True)
     parser.add_argument('--receiver', dest='receiver', default=None, required=True)
     parser.add_argument('--download-dir', dest='download_dir', default=GSIS_DEFAULTS['download_dir'], required=False)
     parser.add_argument('--text', dest='text', default=None, required=True)
     parser.add_argument('--url', dest='url', default=GSIS_DEFAULTS['url'], required=False)
-    parser.add_argument('--retries', dest='retries', default=GSIS_DEFAULTS['retries'], required=False)
-    parser.add_argument('--timeoutt', dest='timeout', default=GSIS_DEFAULTS['timeout'], required=False)
+    parser.add_argument('--retries', dest='retries', default=GSIS_DEFAULTS['retries'], type=int, required=False)
+    parser.add_argument('--timeoutt', dest='timeout', default=GSIS_DEFAULTS['timeout'], type=int, required=False)
     parser.add_argument('--filename', dest='filename', default=None, required=False)
     
         
     args = vars(parser.parse_args())
-    gsis = gsisGrabber(  username=args['user']
-                       , password=args['password']
-                       , taxid=args['taxid']
-                       , email=args['email']
-                       , receiver = args['receiver']
-                       , text = args['text']
-                       , download_dir= args['download_dir']
-                       , url = args['url']
-                       , retries = args['retries']
-                       , timeout = args['timeout']
-                       , getCode = None
-                       , filename='test.pdf'
-                       )
-    declaration = gsis.run()
-    print(declaration)
+
+    try:
+        with gsisGrabber(  username   = args['user']
+                           , password   = args['password']
+                           , taxid      = args['taxid']
+                           , email      = args['email']
+                           , receiver   = args['receiver']
+                           , text       = args['text']
+                           , download_dir = args['download_dir']
+                           , url        = args['url']
+                           , retries    = args['retries']
+                           , timeout    = args['timeout']
+                           , getCode    = None
+                           , filename   = args['filename']
+                           ) as gsis:
+            url, declaration = gsis.run()
+            print(url, declaration)
+            
+    except Exception as e:
+        print(e)
+    #finally: 
+    #    gsis.cleanup() # <-ist explicitl
+    #
     
 
 
